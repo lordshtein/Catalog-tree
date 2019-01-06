e21538f8197d71d8b9df2be26634b0afaf0e3adc
lordshtein <lordshtein@gmail.com>
2019-01-06 12:18:27 +0300

    using prettier to format code

--- bin/global.js
+++ bin/global.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
-const catalogTree = require('../lib/index.js');
+const catalogTree = require("../lib/index.js");
 
 if (process.argv.length <= 2) {
- console.log("Usage: " + __filename + " path/to/directory");
- process.exit(-1);
-}
- 
-if (!process.argv[3]) {
- endLevel = 1;
+  console.log("Usage: " + __filename + " path/to/directory");
+  process.exit(-1);
 }
 
-// Print catalog tree in terminal to defined level 
-catalogTree(process.argv[2]," ",process.argv[3]);
+if (!process.argv[3]) {
+  endLevel = 1;
+}
+
+// Print catalog tree in terminal to defined level
+catalogTree(process.argv[2], " ", process.argv[3]);
